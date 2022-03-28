58d308ff5e5bb6e2bfe7215926abaa2f03f3a753
KimaniKibuthu <32309808+KimaniKibuthu@users.noreply.github.com>
2022-03-28 22:47:16 +0300

    updated files

--- Day 20 - Building a snake game/snake.py
+++ Day 20 - Building a snake game/snake.py
@@ -1,5 +1,6 @@
 from turtle import Turtle, Screen
 import time
+from snake_class import Snake
 
 
 
@@ -8,32 +9,20 @@ screen.bgcolor('black')
 screen.title('Snake Xenzia')
 screen.tracer(0)
 
-segments = []
-starting_positions = [(0, 0), (-20, 0), (-40, 0)]
-for position in starting_positions:
-    segment = Turtle('square')
-    segment.color('white')
-    segment.penup()
-    segment.goto(position)
-    segments.append(segment)
+snake = Snake()
+
+screen.listen()
+screen.onkey(snake.up, 'Up')
+screen.onkey(snake.down, 'Down')
+screen.onkey(snake.lefty, 'Left')
+screen.onkey(snake.righty, 'Right')
 
 game_is_on = True
 while game_is_on:
     screen.update()
     time.sleep(0.1)
-    for segment_number in range(len(segments)-1, 0, -1):
-        new_x = segments[segment_number-1].xcor()
-        new_y = segments[segment_number-1].ycor()
-        segments[segment_number].goto(new_x, new_y)
-
-    segments[0].forward(20)
-
-    
-
-    
-
-
-    
+    snake.move()
+ 
 
 screen.exitonclick()
 
--- Day 20 - Building a snake game/snake_class.py
+++ Day 20 - Building a snake game/snake_class.py
@@ -1,4 +1,42 @@
+from turtle import Turtle, Screen
+
+POSITIONS = [(0, 0), (-20, 0), (-40, 0)]
+DISTANCE = 20
+
 
 class Snake:
-    def __init__(self, positions):
-        self.positions = []
+    def __init__(self):
+        self.segments = []
+        self.create_snake()
+
+    def create_snake(self):
+        for position in POSITIONS:
+            segment = Turtle('square')
+            segment.color('white')
+            segment.penup()
+            segment.goto(position)
+            self.segments.append(segment)
+    
+    def move(self):
+        for segment_number in range(len(self.segments)-1, 0, -1):
+            new_x = self.segments[segment_number-1].xcor()
+            new_y = self.segments[segment_number-1].ycor()
+            self.segments[segment_number].goto(new_x, new_y)
+
+        self.segments[0].forward(DISTANCE)
+
+    def up(self):
+        self.segments[0].left(90)
+        self.move()
+    
+    def down(self):
+        self.segments[0].right(90)
+        self.move()
+    
+    def lefty(self):
+        self.segments[0].left(90)
+        self.move()
+    
+    def righty(self):
+        self.segments[0].right(90)
+        self.move()
